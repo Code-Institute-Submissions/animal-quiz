feccc9e05491fee343ce0893523dd513207664e7
Debra Wolford <debra.wolford@gmail.com>
2020-04-07 16:40:48 +0200

    Added initial js code to get immediate feedback on each question

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -23,10 +23,22 @@ function init() {
 $(document).ready(function() {
   //Hides all questions when page is loaded
   $(".quizquestion").hide();
+  //Hides all feedback initially
+  $(".feedback").hide();
   //Shows first question
   $("#question1").show();
 
+  //on click event for when a radio button is selected
+  $("input[type=radio]").click(function() {
+    //variable that determines the question the user is currently on
+    var currentQuestion = $(this).parents("form:first").data("question");
+    //runs the checkAnswer function to show feedback
+    checkAnswer("" + currentQuestion + "")
+  });
 
+  //on click event for when a next (submit) button is clicked. It runs the recordAnswer function
+  //which will increase the score by 1 if the user selected the correct answer. And it hides the
+  //current question and shows the next question.
   $(".quizquestion #submit").click(function() {
     var currentQuestion = $(this).parents("form:first").data("question");
     var nextQuestion = $(this).parents("form:first").data("question") + 1;
@@ -37,16 +49,29 @@ $(document).ready(function() {
   });
 });
 
-//calculate score
+//calculate scores by recording each answer and adding 1 to the total score if correct
 function recordAnswer(x) {
   var submittedAnswer = $("input[name=question" + x + "]:checked").val();
   if (submittedAnswer === sessionStorage.getItem("answer" + x + "")) {
     score++;
   };
+  //returns total score after the last question
   if (x == total) {
     $("#results").html(`<h3>You got ${score} out of ${highScore} correct!</h3><a href="index.html">Try again!</a>`);
   };
   return false;
 }
+
+//shows feedback below radio buttons when selecting a radio button
+function checkAnswer(x) {
+  var checkedAnswer = $("input[name=question" + x + "]:checked").val();
+  if (checkedAnswer === sessionStorage.getItem("answer" + x + "")) {
+    $("#feedback" + x + "-correct").show();
+  } else {
+    $("#feedback" + x + "-incorrect").show();
+  }
+  return false;
+};
+
 //wait for page to load completely before starting init function
 window.addEventListener("load", init, false);

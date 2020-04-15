9200c8f47aa8aa8e052f73620f4b5fdd4d32b4e5
Debra Wolford <debra.wolford@gmail.com>
2020-04-15 08:40:41 +0200

    Added more detailed comments and added customized feedback for question8

--- assets/js/question-8.js
+++ assets/js/question-8.js
@@ -2,8 +2,9 @@
 // and stores them in the constant "options" so we can work with them later.
 const options = Array.from(document.getElementsByClassName("option"));
 
+//an empty object where the randomly generated map info will be stored
 let selectedMap = {};
-//an empty array where all the data from mapQuestions will be stored in
+//an empty array where all the data from mapInfo will be stored
 let possibleMaps = [];
 
 let mapInfo = [{
@@ -54,11 +55,49 @@ function getMapDetails() {
   var mapInfoIndex = Math.floor(Math.random() * possibleMaps.length);
   selectedMap = possibleMaps[mapInfoIndex];
 
+  /* Changes the text in each <li><label> of question 8 to the corresponding
+  value from the selectedMap variable by grabbing the number from the data-number
+  HTML attribute and putting it after the string "option" to create the key.
+  For example: data-number: 1 will correspond to option1 in mapInfo. */
   options.forEach(option => {
     var number = option.dataset["number"];
     option.innerText = selectedMap["option" + number]
   });
+
+  /* An if else statement which will change the correct feedback
+  depending on which map was generated. As option2 is always the
+  correct answer, the animal variable is set to be equal to the
+  value of "option2" in the currently generated map. Then the if
+  statements check which animal that is and produce the correct feedback
+  accordingly. */
+  let animal = selectedMap["option2"];
+  if (animal == "Platypuses") {
+    $("#feedback8-correct").html(`<div class="text-center">
+    <h4 class="correct">Correct!</h4>
+    <p><img src="assets/images/gifs/platypus.gif" alt="Platypus walking" class="img-fluid mx-auto"></p>
+    <p>Mother hens are amazing moms even before their babies hatch. They talk to their egg and listen as their chick peeps back at them from inside.</p>
+    </div>`);
+  } else if (animal == "Lemurs") {
+    $("#feedback8-correct").html(`<div class="text-center">
+    <h4 class="correct">Correct!</h4>
+    <p><img src="assets/images/gifs/lemur.gif" alt="Lemurs walking" class="img-fluid mx-auto"></p>
+    <p>Mother hens are amazing moms even before their babies hatch. They talk to their egg and listen as their chick peeps back at them from inside.</p>
+    </div>`);
+  } else if (animal == "Kangaroo rats") {
+    $("#feedback8-correct").html(`<div class="text-center">
+    <h4 class="correct">Correct!</h4>
+    <p><img src="assets/images/gifs/kangaroo-rat.gif" alt="Kangaroo rat hopping along" class="img-fluid mx-auto"></p>
+    <p>Mother hens are amazing moms even before their babies hatch. They talk to their egg and listen as their chick peeps back at them from inside.</p>
+    </div>`);
+  } else if (animal == "Zacatuche, or rabbit of the volcanoes") {
+    $("#feedback8-correct").html(`<div class="text-center">
+    <h4 class="correct">Correct!</h4>
+    <p><img src="assets/images/gifs/zacatuche.gif" alt="Zacatuche" class="img-fluid mx-auto"></p>
+    <p>Mother hens are amazing moms even before their babies hatch. They talk to their egg and listen as their chick peeps back at them from inside.</p>
+    </div>`);
+  }
   initMap();
+
 };
 
 /* Google map function code initially taken from developers.google.com
